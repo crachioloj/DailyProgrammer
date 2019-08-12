12f6a7a897ab0772508d14f094cbb877de446388
Joseph Crachiolo <crachioloj@gmail.com>
2019-08-12 18:47:07 -0400

    Add UI

---
+++ Easy/TextToMorse/TextToMorseUI/MorseCodeViewer.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace TextToMorseUI
+{
+    partial class MorseCodeViewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.lblInput = new System.Windows.Forms.Label();
+            this.lblMorse = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtInput.Location = new System.Drawing.Point(12, 35);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(351, 20);
+            this.txtInput.TabIndex = 0;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutput.Location = new System.Drawing.Point(12, 90);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.Size = new System.Drawing.Size(351, 20);
+            this.txtOutput.TabIndex = 1;
+            this.txtOutput.TabStop = false;
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Location = new System.Drawing.Point(12, 19);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(55, 13);
+            this.lblInput.TabIndex = 1;
+            this.lblInput.Text = "Input Text";
+            // 
+            // lblMorse
+            // 
+            this.lblMorse.AutoSize = true;
+            this.lblMorse.Location = new System.Drawing.Point(12, 74);
+            this.lblMorse.Name = "lblMorse";
+            this.lblMorse.Size = new System.Drawing.Size(64, 13);
+            this.lblMorse.TabIndex = 1;
+            this.lblMorse.Text = "Morse Code";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSubmit.Location = new System.Drawing.Point(288, 161);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 2;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.BtnSubmit_Click);
+            // 
+            // MorseCodeViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 196);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.lblMorse);
+            this.Controls.Add(this.lblInput);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtInput);
+            this.Name = "MorseCodeViewer";
+            this.ShowIcon = false;
+            this.Text = "Morse Code";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.Label lblMorse;
+        private System.Windows.Forms.Button btnSubmit;
+    }
+}
+
